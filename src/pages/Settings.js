635c56ec42f58860c2d835aaa203009f8655c231
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-08-05 17:51:42 -0300

    Settings button created - Req3

---
+++ src/pages/Settings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h2 data-testid="settings-title">Settings</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Settings;
