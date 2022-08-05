ea8a100de131804f0eaffe34b157fab1836c3c4a
Cássio Carvalho <cassio.acarvalho@gmail.com>
2022-08-05 18:18:18 -0300

    Header initialized - Req 5

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Game extends React.Component {
   render() {
     return (
       <div>
-        Game Page
+        <Header />
       </div>
     );
   }
