f9f9a78adaef1a03e9989d6154d0cffd8a93c4c1
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-05 15:22:06 -0300

    starting requirement 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>oi</p>
+        <p>Req1</p>
       </header>
     </div>
   );
