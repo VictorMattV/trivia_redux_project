5869387c8796c6e0f8fe11f22a943853da259d0f
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-05 16:32:09 -0300

    add Login page - Requirement 1 ok

---
+++ src/pages/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import logo from '../trivia.png';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      username: '',
+      isDisabled: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.validateInputs);
+  }
+
+  validateInputs = () => {
+    const { email, username } = this.state;
+    const minLength = 5;
+    if (email.length > minLength && username.length > minLength) {
+      this.setState({
+        isDisabled: false,
+      });
+    }
+  }
+
+  render() {
+    const { email, username, isDisabled } = this.state;
+    return (
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
+        <label htmlFor="email">
+          <input
+            type="email"
+            data-testid="input-gravatar-email"
+            id="email"
+            name="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="username">
+          <input
+            type="name"
+            data-testid="input-player-name"
+            id="username"
+            name="username"
+            value={ username }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isDisabled }
+        >
+          Play
+        </button>
+      </header>
+    );
+  }
+}
+
+export default Login;
