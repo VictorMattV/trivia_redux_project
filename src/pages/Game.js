3b4351f7967d266c38e1bfaddf3c0661cebb0712
edmcorrea <edm.correa@hotmail.com>
2022-08-06 17:21:10 -0300

    duvidas no quesito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,75 +1,78 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import fetchQuestions from '../services/fetchQuestions';
 
 class Game extends React.Component {
   state = {
-    // category: '',
     questions: [],
     index: 0,
     loading: true,
-    // type: '',
-    // correctAnswer: '',
-    // incorrectAnswer: '',
+    logout: false,
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.fetchCalling();
+  }
+
+  fetchCalling = async () => {
     const resultsFetch = await fetchQuestions();
     const questions = resultsFetch.results;
+    if (!questions.length) {
+      localStorage.removeItem('token');
+      this.setState({ logout: true });
+    }
     this.setState({
       questions }, () => {
       this.setState({ loading: false });
-      // const { category, question, type, correct_answer, incorrect_answers } = questions.results;
-      // category: questions.results.category,
-      // question: questions.results.question,
-      // type: questions.results.type,
-      // correctAnswer: questions.results.correct_answer,
-      // incorrectAnswer: questions.results.incorrect_answers,
     });
   }
 
-  onClick = () => {
+  handleClick = () => {
     const { index, questions } = this.state;
     const arrLength = questions.length - 1;
-    if (index <= arrLength) {
+    if (index < arrLength) {
       this.setState({ index: index + 1 });
+    } else {
+      this.setState({ index });
     }
   }
 
   render() {
-    const { questions, index, loading } = this.state;
+    const { questions, index, loading, logout } = this.state;
+    if (logout) {
+      return <Redirect to="/" />;
+    }
     return (
       <div>
         <Header />
-        {loading
-          ? <p>Loading</p>
-          : (
-            <section>
-              <h3 data-testid="question-category">{questions[index].category}</h3>
-              <p data-testid="question-text">{questions[index].question}</p>
-              <div data-testid="answer-options">
+        {!loading && (
+          <section>
+            <h3 data-testid="question-category">{questions[index].category}</h3>
+            <p data-testid="question-text">{questions[index].question}</p>
+            <div data-testid="answer-options">
+              <button
+                data-testid="correct-answer"
+                type="button"
+                onClick={ this.handleClick }
+              >
+                {questions[index].correct_answer}
+              </button>
+
+              {questions[index].incorrect_answers.map((elem, i) => (
                 <button
-                  data-testid="correct-answer"
+                  data-testid={ `wrong-answer-${index}` }
                   type="button"
-                  onClick={ this.onClick }
+                  key={ i }
+                  onClick={ this.handleClick }
                 >
-                  {questions[index].correct_answer}
+                  {elem}
                 </button>
+              ))}
 
-                {questions[index].incorrect_answers.map((elem, i) => (
-                  <button
-                    data-testid={ `wrong-answer-${index}` }
-                    type="button"
-                    key={ i }
-                    onClick={ this.onClick }
-                  >
-                    {elem}
-                  </button>
-                ))}
-
-              </div>
-            </section>
-          )}
+            </div>
+          </section>
+        )}
       </div>
     );
   }
--- src/services/fetchQuestions.js
+++ src/services/fetchQuestions.js
@@ -1,6 +1,7 @@
 const fetchQuestions = async () => {
   const token = localStorage.getItem('token');
   const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  console.log(token);
   const response = await fetch(url);
   const json = await response.json();
   return json;
