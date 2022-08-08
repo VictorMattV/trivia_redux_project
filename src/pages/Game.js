40753ab0d1731b4fddfaaee0e64f6769e303a8a3
edmcorrea <edm.correa@hotmail.com>
2022-08-08 17:56:53 -0300

    adicionando alterações do randomico

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,9 +4,36 @@ import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
+// const random3 = 3;
+// const random4 = 4;
+// const random5 = 5;
+
 class Game extends React.Component {
   state = {
     index: 0,
+    questionsAll: [],
+  }
+
+  // componentDidUpdate() {
+  //   const { loading } = this.props;
+  //   const { index } = this.state;
+  //   if (!loading) {
+  //     this.randomAnswer(index);
+  //   }
+  // }
+
+  randomAnswer = (index) => {
+    const { questions } = this.props;
+    console.log(questions);
+    const questionsInc = questions[index].incorrect_answers;
+    const questionsAll = questionsInc.concat(questions[index].correct_answer);
+    console.log(questionsAll);
+    for (let i = 0; i < questionsAll.length; i += 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [questionsAll[i], questionsAll[j]] = [questionsAll[j], questionsAll[i]];
+    }
+    this.setState({ questionsAll });
+    console.log(questionsAll);
   }
 
   handleClick = () => {
@@ -14,44 +41,60 @@ class Game extends React.Component {
     const { questions } = this.props;
     const arrLength = questions.length - 1;
     if (index < arrLength) {
-      this.setState({ index: index + 1 });
+      this.setState({ index: index + 1 }, () => this.randomAnswer(index));
     } else {
-      this.setState({ index });
+      this.setState({ index }, () => this.randomAnswer(index));
     }
   }
 
   render() {
-    const { index } = this.state;
+    const { index, questionsAll } = this.state;
     const { loading, questions, logout } = this.props;
+    if (!loading) {
+      this.randomAnswer(index);
+    }
+    // this.randomAnswer(index);
+    // console.log(questions);
+    // console.log(arrRandom);
     if (logout) {
       return <Redirect to="/" />;
     }
-    console.log('oi');
     return (
       <div>
         <Header />
-        {!loading && (
+        {!loading && questions.length && (
           <section>
             <h3 data-testid="question-category">{questions[index].category}</h3>
             <p data-testid="question-text">{questions[index].question}</p>
-            <div data-testid="answer-options">
-              <button
+            <div data-testid="answer-options" className="answer-options">
+              {/* <button
                 data-testid="correct-answer"
                 type="button"
                 onClick={ this.handleClick }
+                // className={ `order-${arrRandom[0]}` }
               >
                 {questions[index].correct_answer}
-              </button>
-
-              {questions[index].incorrect_answers.map((elem, i) => (
-                <button
-                  data-testid={ `wrong-answer-${index}` }
-                  type="button"
-                  key={ i }
-                  onClick={ this.handleClick }
-                >
-                  {elem}
-                </button>
+              </button> */}
+              {questionsAll.map((elem, i) => (
+                (elem === questions[index].correct_answer)
+                  ? (
+                    <button
+                      data-testid="correct-answer"
+                      type="button"
+                      onClick={ this.handleClick }
+                    >
+                      {elem}
+                    </button>)
+                  : (
+                    <button
+                      data-testid={ `wrong-answer-${index}` }
+                      type="button"
+                      key={ i }
+                      onClick={ this.handleClick }
+                    >
+                      {elem}
+                    </button>
+                  )
               ))}
 
             </div>
