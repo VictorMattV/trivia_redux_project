ff607d95de353b7ce49a727700dfeecdad77468f
Erik Yamamoto <erik.ymmt@gmail.com>
2022-08-11 20:14:33 -0300

    add ranking css

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import fetchGravatar from '../services/fetchGravatar';
+import '../styles/Ranking.css';
 
 class Ranking extends React.Component {
   state = {
@@ -29,7 +30,7 @@ class Ranking extends React.Component {
     const { history } = this.props;
     const { userRanking } = this.state;
     return (
-      <>
+      <div className="rankingPage">
         <h1 data-testid="ranking-title">Ranking</h1>
         <button
           type="button"
@@ -38,20 +39,27 @@ class Ranking extends React.Component {
         >
           Go Home
         </button>
-        {userRanking.length
-          ? (userRanking.map((user, index) => (
-            <div key={ index }>
-              <img
-                src={ this.getGravatarImage(user.email) }
-                alt="header-profile"
-                data-testid="header-profile-picture"
-              />
-              <h3 data-testid={ `player-name-${index}` }>{user.username}</h3>
-              <h3 data-testid={ `player-score-${index}` }>{user.score}</h3>
-            </div>
-          )))
-          : <p>não há Ranking</p>}
-      </>
+        <div className="rankingContainer">
+          {userRanking.length
+            ? (userRanking.map((user, index) => (
+              <div key={ index } className="playerRankConteiner">
+                <div className="playerRank">
+                  <h4>{index + 1}</h4>
+                  <div className="playerRankInfo">
+                    <img
+                      src={ this.getGravatarImage(user.email) }
+                      alt="header-profile"
+                      data-testid="header-profile-picture"
+                    />
+                    <h3 data-testid={ `player-name-${index}` }>{user.username}</h3>
+                    <h3 data-testid={ `player-score-${index}` }>{user.score}</h3>
+                  </div>
+                </div>
+              </div>
+            )))
+            : <p>não há Ranking</p>}
+        </div>
+      </div>
 
     );
   }
